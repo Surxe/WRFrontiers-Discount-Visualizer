e43cbf2845f2c5207f01537f81edbd8ea8b880da
Surxe <119145352+Surxe@users.noreply.github.com>
2026-05-29 18:54:55 -0500

    add base url to asset hrefs, fix symlink relative pathing (again)

--- src/frontend/astro.config.mjs
+++ src/frontend/astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
-	site: 'https://Surxe.github.io',
+	site: 'https://surxe.github.io',
 	base: '/WRFrontiers-Discount-Visualizer/'
 });
